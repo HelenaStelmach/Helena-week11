ba99af7d631835bc45b1c2d8079bff863e354dca
Helena Stelmach <helena.stelmach@tktk.ee>
2026-05-16 12:51:22 +0300

    week11

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const { Pool } = require('pg');
 const itemsPool = new Pool({
-    user: 'postgres',
-    password: 'admin1234',
-    host: 'localhost',
-    port: 5432,
-    database: 'myrecipes'
+    connectionString: process. env. DBConnetionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    //user: 'postgres',
+    //password: 'admin1234',
+    //host: 'localhost',
+    //port: 5432,
+    //database: 'myrecipes'
 });
 
 module.exports = itemsPool;
