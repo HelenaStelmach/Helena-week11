68f8b5fa1cd72ebeee20741d75ed77a103e9e6db
Helena Stelmach <helena.stelmach@tktk.ee>
2026-05-16 23:02:18 +0300

    fix env database url

--- db.js
+++ db.js
@@ -1,16 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const { Pool } = require('pg');
+
 const itemsPool = new Pool({
-    connectionString: process. env. DBConnetionString,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
-    //user: 'postgres',
-    //password: 'admin1234',
-    //host: 'localhost',
-    //port: 5432,
-    //database: 'myrecipes'
 });
 
 module.exports = itemsPool;
