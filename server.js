7ef30ad5744ea0653c9e339cb51aa250b5694401
Helena Stelmach <helena.stelmach@tktk.ee>
2026-05-16 22:42:19 +0300

    week11

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use('/random', randomRouter);
 
 const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}.`);
 });
